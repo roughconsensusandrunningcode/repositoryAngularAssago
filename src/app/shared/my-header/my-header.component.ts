07a91e8bae18eea2b30d00d15d29f051b19d750b
sorrentmutie <salvatore.sorrentino@live.com>
2019-11-21 17:04:46 +0100

    observables

--- src/app/shared/my-header/my-header.component.ts
+++ src/app/shared/my-header/my-header.component.ts
@@ -1,5 +1,7 @@
 import { Component} from '@angular/core';
 import { Router } from '@angular/router';
+import { of, Observable, range } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-my-header',
@@ -7,6 +9,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./my-header.component.css']
 })
 export class MyHeaderComponent {
+
+  constructor() {
+  //  const myObservable = of(1, 2, 3, 4, 5, 6 , 7, 8, 9, 10);
+  //  const myObservable = new Observable(this.generatoreDiSequenze);
+    const myObservable = range(0, 10000);
+
+    const mySubscriber = {
+      next: valoreEmesso =>  console.log('valore: ' + valoreEmesso),
+      complete: () => console.log('flusso terminato'),
+      error: (error) => console.log(error)
+    };
+
+    myObservable.pipe(filter( x => x % 2 === 0)).subscribe(mySubscriber);
+  }
+
+   private generatoreDiSequenze(observer: any): any {
+    observer.next(1);
+    observer.next('a');
+    observer.next(3);
+    observer.complete();
+    return {unsubscribe(){}};
+   }
 
 
 
