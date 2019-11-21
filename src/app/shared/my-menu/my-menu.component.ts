779fe137e2e463cbd52f042e22d3032f3aeb47ee
sorrentmutie <salvatore.sorrentino@live.com>
2019-11-21 11:42:13 +0100

    menu

--- src/app/shared/my-menu/my-menu.component.ts
+++ src/app/shared/my-menu/my-menu.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from './menu-item';
 
 @Component({
   selector: 'app-my-menu',
   templateUrl: './my-menu.component.html',
   styleUrls: ['./my-menu.component.css']
 })
-export class MyMenuComponent implements OnInit {
+export class MyMenuComponent {
 
-  constructor() { }
+  menu: MenuItem[] = [];
 
-  ngOnInit() {
+  constructor() {
+    this.menu = this.getMenu();
   }
+
+  getMenu(): MenuItem[] {
+    return [
+      {text: 'Home', link: '#'},
+      {text: 'Products', link: '#'},
+      {text: 'Users', link: '#'},
+      {text: 'About', link: '#'},
+      {text: 'Contacts', link: '#'},
+    ];
+  }
+
 
 }
